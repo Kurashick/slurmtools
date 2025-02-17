e92e0cc6b48a3b9f461d137b0f808a456eaefc56
Kurashiki Kazuma <157878377+Kurashick@users.noreply.github.com>
2025-02-17 17:28:49 +0900

    Update slurmsh.py

--- slurmsh.py
+++ slurmsh.py
@@ -5,7 +5,10 @@
 
 import os
 from datetime import datetime as dt
-from subprocess import check_call
+import subprocess
+import time
+import datetime
+import warnings
 
 
 class SlurmSh:
@@ -42,6 +45,14 @@ class SlurmSh:
         kwargs["e"]=os.path.join(self.outdir, "%x.%j.err")
         for k, v in kwargs.items():
             self.batch.append("#SBATCH -"+k+" "+v )
+            
+    def add_batch(self,lines:list):
+        """
+        Slurmのオプションを追加する.
+        lines: 追加するオプションをリスト形式で指定する.#SBATCH は省略する.
+        """
+        for x in lines:
+            self.batch.append("#SBATCH " + x)
 
     def set_command(self,*args):
         """
@@ -70,13 +81,115 @@ class SlurmSh:
         self.path=os.path.join(self.dir, self.filename+'.sh')
         with open(self.path, mode='w') as f:
             f.write('\n'.join(lines))
+            
+    def isRunning(self):
+        """
+        Check submitted job is still Running
+        """
+        def runCmd(exe):
+            p = subprocess.Popen(exe, stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT,
+                                    universal_newlines=True)
+            while True:
+                line = p.stdout.readline()
+                if line != '':
+                    # the real code does filtering here
+                    yield line.rstrip()
+                else:
+                    break
+        jobs = runCmd('squeue')
+        columns = None
+        for line in jobs:
+            if str(self.jobNum) in line:
+                columns = line.split()
+                #print(line)
+        if columns is not None:
+            return True
+        else:
+            return False
     
-    def submit_sh(self):
+    def get_exit_status(self):
+        def runCmd(exe):
+            p = subprocess.Popen(exe, stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT,
+                                    universal_newlines=True)
+            while True:
+                line = p.stdout.readline()
+                if line != '':
+                    # the real code does filtering here
+                    yield line.rstrip()
+                else:
+                    break
+                
+        jobs=runCmd("qs")
+        columns = None
+        for line in jobs:
+            if str(self.jobNum) in line:
+                columns = line.split()
+        if columns is not None:
+            return columns[3]
+        else:
+            return False
+    
+    def waitjob(self, checkinterval=5):
+        """
+        ジョブの完了を待つ.
+        jobNum: 提出したジョブの番号
+        checkinterval: ジョブの完了を確認する間隔
+        """
+        
+        while self.isRunning():
+            try:
+                time.sleep(checkinterval)
+            except Exception as e:
+                print(e)
+                self.quitjob()
+                exit()
+        
+        result = self.get_exit_status()
+        return result
+        
+            
+
+    
+    def quitjob(self):
+        """
+        ジョブをキャンセルする.
+        """
+        cmd1 = "scancel " + str(self.jobNum)
+        out1 = subprocess.Popen(cmd1, shell=True,
+                               stdout=subprocess.PIPE, universal_newlines=True)
+        out1 = out1.communicate()[0]
+        out2 = subprocess.Popen("Y", shell=True,
+                               stdout=subprocess.PIPE, universal_newlines=True)
+        print(f"Job {self.jobNum} is cannceled.")
+        
+        
+    
+    def submit_sh(self, wait=False, checkinterval=5):
         """
         シェルスクリプトを作成して提出する.
+        wait: ジョブの完了を待つかどうか
         """
         self.make_sh()
         cmd='sbatch ' + self.path
-        check_call(cmd.split())
+        
+        out = subprocess.Popen(cmd, shell=True,
+                               stdout=subprocess.PIPE, universal_newlines=True)
+        out = out.communicate()[0]
+        self.jobNum = int(out.split()[-1])
+        dt_st = datetime.datetime.now()
+        print( str(dt_st) +
+              f'\nQueue number is {self.jobNum}\n')
 
-
+        if wait:
+            self.waitjob(checkinterval)
+            result=self.get_exit_status()
+            dt_end = datetime.datetime.now()
+            print(str(dt_end) +
+                "\nJob "+ str(self.jobNum)+" ended with status "+result + 
+                f"\nelapsed time: {dt_end-dt_st}\n")
+            
+            if result == "FAIL":
+                print(f"{self.jobNum} has exited with error")
+                exit()
